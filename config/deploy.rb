4134301db06f00ed28e1c87c257954016c5e2250
Cristian Eigel <cristian.eigel@icloud.com>
2015-12-05 10:43:52 +0100

    Set rvm correctly in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 lock '3.4.0'
 
-server '95.85.3.84', port: 3000, roles: [:web, :app, :db], primary: true
+server '95.85.3.84', roles: [:web, :app, :db], primary: true
 set :repo_url, 'git@github.com:ceigel/tictactoe.git'
 set :application, 'tictactoe'
 set :user, 'deploy'
 
-set puma_threads: [4, 16]
-set puma_workers: 0
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 
 set :pty,             true
 set :use_sudo,        false
@@ -18,7 +18,8 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, {:forward_agent => true, user: fetch(:user)}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
